0ae48dbae9e4cffd1634830a5691efae9a336170
hugu1026 <hugu1026@gmail.com>
2018-06-22 19:20:26 +0900

    [add]NPCのID、NPCが持っているクエスト一覧を渡すユーティリティの追加

--- src/main/java/hugu1026/com/github/phantasyquest/util/QuestYAMLReaderUtil.java
+++ src/main/java/hugu1026/com/github/phantasyquest/util/QuestYAMLReaderUtil.java
@@ -10,25 +10,61 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuestYAMLReaderUtil {
-    public static File getQuestFile(String questName) {
+    public static File getQuestFile(String questFileName) {
         File questData = new File(Bukkit.getServer().getPluginManager().getPlugin("PhantasyQuest").getDataFolder(), File.separator + "quests");
-        return new File(questData, File.separator + questName + ".yml");
+        return new File(questData, File.separator + questFileName + ".yml");
     }
 
-    public static FileConfiguration getQuestData(String questName) {
+    public static FileConfiguration getQuestData(String questFileName) {
         File questDatas = new File(Bukkit.getServer().getPluginManager().getPlugin("PhantasyQuest").getDataFolder(), File.separator + "quests");
-        File questFile = new File(questDatas, File.separator + questName + ".yml");
+        File questFile = new File(questDatas, File.separator + questFileName + ".yml");
         return YamlConfiguration.loadConfiguration(questFile);
     }
 
-    public static List<String> getAllQuestsNames() {
+    public static List<String> getAllQuestsFilesNames() {
         File questFile = new File(Bukkit.getServer().getPluginManager().getPlugin("PhantasyQuest").getDataFolder(), File.separator + "quests");
         List<File> questFiles = Arrays.asList(questFile.listFiles());
-        List<String> questNames = new ArrayList<>();
+        List<String> questFilesNames = new ArrayList<>();
         questFiles.forEach(file -> {
             String questName = file.getName().replace("plugins\\PhantasyQuest\\quests\\", "");
-            questNames.add(questName);
+            questName = questName.replace(".yml", "");
+            questFilesNames.add(questName);
         });
-        return questNames;
+        return questFilesNames;
+    }
+
+    public static String getQuestName(String questFileName) {
+        return getQuestData(questFileName).getString("Name");
+    }
+
+    public static List<String> getNPCNames(String questFileName) {
+        List<String> NPCs = getQuestData(questFileName).getStringList("NPCs");
+        List<String> NPCNames = new ArrayList<>();
+        NPCs.forEach(NPC -> {
+            String strings[] = NPC.split(":");
+            NPCNames.add(strings[0]);
+        });
+        return NPCNames;
+    }
+
+    public static List<Integer> getNPCIDs(String questFileName) {
+        List<String> NPCs = getQuestData(questFileName).getStringList("NPCs");
+        List<Integer> NPCIDs = new ArrayList<>();
+        NPCs.forEach(NPC -> {
+            String strings[] = NPC.split(":");
+            NPCIDs.add(Integer.valueOf(strings[1]));
+        });
+        return NPCIDs;
+    }
+
+    public static String getVaildQuestsFilesNames(int NPCId) {
+        List<String> allQuestsFileNames = getAllQuestsFilesNames();
+        for (String questFileName : allQuestsFileNames) {
+            List<Integer> NPCs = getNPCIDs(questFileName);
+            if (NPCs.contains(NPCId)) {
+                return questFileName;
+            }
+        }
+        return null;
     }
 }
