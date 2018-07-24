8b5730f9df602450ae141327b3c7be60293a845d
hugu1026 <hugu1026@gmail.com>
2018-07-24 15:27:43 +0900

    [change]タグの確認方法を変更

--- src/main/java/hugu1026/com/github/phantasyquest/quest/condition/HasTag.java
+++ src/main/java/hugu1026/com/github/phantasyquest/quest/condition/HasTag.java
@@ -3,8 +3,6 @@ package hugu1026.com.github.phantasyquest.quest.condition;
 import hugu1026.com.github.phantasyquest.util.PlayerQuestDataUtil;
 import org.bukkit.entity.Player;
 
-import java.util.List;
-
 public class HasTag extends Condition {
     public HasTag(String conditionID) {
         super(conditionID);
@@ -13,11 +11,7 @@ public class HasTag extends Condition {
     @Override
     public boolean checkMeetCondition(Player player) {
         String tag = getArg(1);
-        List<String> havingTags = PlayerQuestDataUtil.getTags(player);
 
-        for (String havingTag : havingTags) {
-            return havingTag.equals(tag);
-        }
-        return false;
+        return PlayerQuestDataUtil.getTags(player).contains(tag);
     }
 }
